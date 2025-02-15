098a05284f19bdc5ba831697dc499a241282f109
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2025-02-15 11:59:06 +0300

    album controller

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -133,5 +133,5 @@ export const deleteAlbum = async (req, res, next) => {
 };
 
 export const checkAdmin = async (req, res, next) => {
-  res.status(200).json({ message: "Admin verified" });
+  res.status(200).json({ admin: true });
 }
